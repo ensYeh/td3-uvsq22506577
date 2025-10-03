05b928ba78af81417cacf3fa5ce7170b9a2037d5
DEBBIHI FATIHA <raniadebbihi@gmail.com>
2025-10-03 12:10:48 +0200

    Ajout le constructeur pour charger la base de données

--- src/main/java/fr/uvsq/cprog/collex/Dns.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns.java
@@ -1,5 +1,32 @@
 package fr.uvsq.cprog.collex;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+
 public class Dns {
+     private Map<String, DnsItem> nomToItem; 
+    private Map<String, DnsItem> ipToItem;  
+    private Path filePath;                   
+
+    public Dns(String fileName) throws IOException {
+        this.filePath = Paths.get(fileName);
+        nomToItem = new HashMap<>();
+        ipToItem = new HashMap<>();
+        loadFromFile();
+    }
+
+
     
+
+
+
+
+
+
+
+
+
+
+
 }
