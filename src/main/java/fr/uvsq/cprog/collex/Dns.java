40aaaa091823d5768099e6d6b127e61b5a76baa3
DEBBIHI FATIHA <raniadebbihi@gmail.com>
2025-10-03 12:19:32 +0200

    ajout methode privé pour charger la base depuis le fichier

--- src/main/java/fr/uvsq/cprog/collex/Dns.java
+++ src/main/java/fr/uvsq/cprog/collex/Dns.java
@@ -5,16 +5,38 @@ import java.nio.file.*;
 import java.util.*;
 
 public class Dns {
+
+
      private Map<String, DnsItem> nomToItem; 
     private Map<String, DnsItem> ipToItem;  
-    private Path filePath;                   
-
+    private Path filePath;
+    
+       @param fileName nom du fichier de stockage
+     * @throws IOException si problème de lecture
+     
     public Dns(String fileName) throws IOException {
         this.filePath = Paths.get(fileName);
         nomToItem = new HashMap<>();
         ipToItem = new HashMap<>();
         loadFromFile();
     }
+
+
+    private void loadFromFile() throws IOException {
+        if (!Files.exists(filePath)) return; 
+        List<String> lines = Files.readAllLines(filePath);
+        for (String line : lines) {
+            String[] parts = line.split(" ");
+            if (parts.length == 2) {
+                NomMachine nom = new NomMachine(parts[0]);
+                AdresseIP ip = new AdresseIP(parts[1]);
+                DnsItem item = new DnsItem(nom, ip);
+                nomToItem.put(parts[0], item); 
+                ipToItem.put(parts[1], item);  
+            }
+        }
+    }
+
 
 
     
