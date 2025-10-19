da8ee8c82c636767ae0678a6d57b118f9b9d98da
DEBBIHI FATIHA <raniadebbihi@gmail.com>
2025-10-19 19:51:04 +0200

    recherche un nom à partir d’une Ip

--- src/main/java/fr/uvsq/cprog/collex/RechercherNom.java
+++ src/main/java/fr/uvsq/cprog/collex/RechercherNom.java
@@ -1,5 +1,21 @@
 package fr.uvsq.cprog.collex;
 
-public class RechercherNom {
-    
+public class RechercheNom implements Commande {
+    private AdresseIP adresse;
+    private Dns dns;
+
+    public RechercheNom(Dns dns, AdresseIP adresse) {
+        this.dns = dns;
+        this.adresse = adresse;
+    }
+
+    @Override
+    public void execute() {
+        try {
+            DnsItem item = dns.getItem(adresse);
+            System.out.println(item.getNomMachine());
+        } catch (Exception e) {
+            System.out.println("Adresse IP non trouvée !");
+        }
+    }
 }
