6a4c059e5cd680f28da97e06703356e0f7ead297
DEBBIHI FATIHA <raniadebbihi@gmail.com>
2025-10-19 20:25:39 +0200

    Ajouter un item dans la base de données

--- src/main/java/fr/uvsq/cprog/collex/AjouterItem.java
+++ src/main/java/fr/uvsq/cprog/collex/AjouterItem.java
@@ -1,5 +1,23 @@
 package fr.uvsq.cprog.collex;
 
-public class AjouterItem {
-    
+public class AjouterItem implements Commande {
+    private Dns dns;
+    private NomMachine nomMachine;
+    private AdresseIP adresse;
+
+    public AjouterItem(Dns dns, NomMachine nomMachine, AdresseIP adresse) {
+        this.dns = dns;
+        this.nomMachine = nomMachine;
+        this.adresse = adresse;
+    }
+
+    @Override
+    public void execute() {
+        try {
+            dns.addItem(adresse, nomMachine);
+            System.out.println("Machine ajoutée avec succès !");
+        } catch (Exception e) {
+            System.out.println("ERREUR : " + e.getMessage());
+        }
+    }
 }
